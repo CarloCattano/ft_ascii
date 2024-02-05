68c6b8629e39839a242252d0ff150218a024bbcb
CarloCattano <freevibes@gmail.com>
2024-02-05 11:56:46 +0100

    Testing effects

--- init.c
+++ init.c
@@ -28,7 +28,7 @@ void init_term(term_t *t)
 {
     t->frame = 1;
     t->clear = 0;
-    t->delay = 1e1;
+    t->delay = 1e2;
 
     t->buffer = (char *)calloc(t->size, sizeof(char));
     t->buffer_copy = (char *)calloc(t->size, sizeof(char));
