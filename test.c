98089b3314d53a0a37e46c429c13c25779989cc3
CarloCattano <freevibes@gmail.com>
2024-02-10 15:17:23 +0100

    test, strlen is cutting if null is found, rewrite

--- test.c
+++ test.c
@@ -1,22 +1,6 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "portaudio.h"
+#include <unistd.h>
 
-// gcc main.c -lportaudio -lrt -lm -lasound -ljack -pthread -o pa_out_index
-
-int main() {
-    PaError err;
-    err = Pa_Initialize();
-    // get defaul audio input device 
-    PaDeviceIndex default_input_device = Pa_GetDefaultInputDevice();
-    // list input devices 
-    int numDevices = Pa_GetDeviceCount();
-    for (int i = 0; i < numDevices; i++) {
-        const PaDeviceInfo *deviceInfo = Pa_GetDeviceInfo(i);
-        printf("device %d: %s\n", i, deviceInfo->name);
-    }
-
-
-    printf("default output device index %d", default_input_device);
-    return EXIT_SUCCESS;
+int main()
+{
+    write(1, "Hello,\0 world!\n", 15);
 }
