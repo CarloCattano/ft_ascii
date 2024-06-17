d0de94701d4227dccf5bcf1123666ff8104e4017
CarloCattano <freevibes@gmail.com>
2024-06-17 20:43:49 +0200

    update and organize folders

--- README.md
+++ README.md
@@ -13,14 +13,12 @@ Branches as of today are:
 
 Interested in rendering graphics to terminal using just *ASCII*
 The original idea was to replace mlx library with this, in order to run cub3d on the terminal ( 42 )
-Its a WIP experimenting around this in order to see whats needed.
 
-### The problem / learning opportunity
 There are numerous implementations and literature on this matter, however, they always usually rely libs like
 termios or ncurses. In here however, we are trying to do it the 42 way, without the strict norm rules.
 
 ### Building ( Linux ) 
-a convenient build.sh is provided, if you like to suffer with make, cmake, xzmake, go ahead.
+a convenient build.sh is provided
 
 Controls:
 
--- build.sh
+++ build.sh
@@ -3,18 +3,18 @@
 set -e 
 
 if [ $# -eq 0 ]; then
-    cc -Wall -Wextra -Werror -lm main.c ftascii.c utils.c init.c draw.c keyhooks.c player.c -o ft_ascii
+    cc -Wall -Wextra -Werror -I include/  -lm src/main.c src/ftascii.c src/utils.c src/init.c src/draw.c src/keyhooks.c src/player.c -o ft_ascii
     echo "Built ft_ascii"
 
 elif [ $1 == "run" ]; then
-    cc -Wall -Wextra -Werror -lm main.c ftascii.c utils.c init.c draw.c keyhooks.c player.c -o ft_ascii
+    cc -Wall -Wextra -Werror -I include/ -lm src/main.c src/ftascii.c src/utils.c src/init.c src/draw.c src/keyhooks.c src/player.c -o ft_ascii
     ./ft_ascii
 
 elif [ $1 == "clean" ]; then
     rm ft_ascii -f
     
 elif [ $1 == "debug" ]; then
-    cc -Wall -Wextra -Werror -g -lm main.c ftascii.c utils.c init.c draw.c keyhooks.c player.c -o ft_ascii
+    cc -Wall -Wextra -Werror -I include/ -g -lm src/main.c src/ftascii.c src/utils.c src/init.c src/draw.c src/keyhooks.c src/player.c -o ft_ascii
     if [ $2 == "run" ]; then
         gdb ./ft_ascii
     else
