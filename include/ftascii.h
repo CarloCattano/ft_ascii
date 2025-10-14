606e59dab6a1a5cbd164fe35f8cdae0e6973413e
CarloCattano <freevibes@gmail.com>
2025-10-14 21:21:49 +0200

    remove srand

--- include/ftascii.h
+++ include/ftascii.h
@@ -17,12 +17,10 @@
 // The main structure that holds the data for each "pixel"
 // consisting of a unicode character and a color
 typedef struct s_unicode {
-	char* uni;
-	char* color;
+    char *uni;
+    char *color;
 
 } Pixel;
-
-// clang-format off
 
 /** @brief  Structure to store the terminal data
  *  @param  MAX_COL     Maximum number of columns in the terminal
@@ -36,37 +34,35 @@ typedef struct s_unicode {
  *  @param  draw        Draw flag to draw keep the drawing loop running
  */
 
-typedef struct		term_s 
-{
-	int				MAX_COL;
-	int				MAX_ROW;
-	int				size;
-	char*		 	buffer;
-	Pixel*			pixels;
-	unsigned int	frame;
-	int				clear;
-	unsigned int	delay;
-	bool			draw;
+typedef struct term_s {
+    int          MAX_COL;
+    int          MAX_ROW;
+    int          size;
+    char        *buffer;
+    Pixel       *pixels;
+    unsigned int frame;
+    int          clear;
+    unsigned int delay;
+    bool         draw;
 
-}					term_t;
+} term_t;
 
-
-void			ft_ascii();
-void 			init_term(term_t* t);
-void 			draw(term_t* t, void (*f)(term_t*));
-void 			assign_pix_buff(char* buffer, Pixel* pixels, int size);
-void 			fill_pixel(Pixel* pixels, char* color, char* uni, int i);
-void 			putpix(Pixel* pixels, char* color, char* uni);
-void 			map_pix(term_t* t, int x, int y, char* color, char* uni);
+void ft_ascii();
+void init_term(term_t *t);
+void draw(term_t *t, void (*f)(term_t *));
+void assign_pix_buff(char *buffer, Pixel *pixels, int size);
+void fill_pixel(Pixel *pixels, char *color, char *uni, int i);
+void putpix(Pixel *pixels, char *color, char *uni);
+void map_pix(term_t *t, int x, int y, char *color, char *uni);
 
 //   hooks
-void 			handleKeyPress(char key, term_t* t);
-void 			ft_keyhook(term_t* t);
+void handleKeyPress(char key, term_t *t);
+void ft_keyhook(term_t *t);
 
 //	 utils
-void 			free_all(term_t* t);
-void 			systemExit(term_t* t);
+void free_all(term_t *t);
+void systemExit(term_t *t);
 
-extern char*	all_colors[10];
+extern char *all_colors[10];
 
 #endif
